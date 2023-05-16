a8cc6581512e2f70fd7b5acc4ab80d44b9dfeebd
Pepijn <pepijnlinden@gmail.com>
2023-05-16 16:39:48 +0200

    Made improvements to the cookie quiz

--- Assets/Scripts/Cookies/CookieGameManager.cs
+++ Assets/Scripts/Cookies/CookieGameManager.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CookieGameManager : MonoBehaviour
 {
     public int currentQuestion = 0;
+    public GameObject questionCard;
+    [SerializeField] private TMP_Text quizText;
+    [SerializeField] private TMP_Text storyText;
+    public string[] quizQuestions;
+    public string[] correctAnswers;
+
+
+    public void CheckQuizAnswer(string answer)
+    {
+        if (answer == correctAnswers[currentQuestion])
+        {
+            Debug.Log("Bingo");
+            storyText.text = "Nice!";
+            currentQuestion++;
+            questionCard.SetActive(false);
+        }
+        else if(answer != correctAnswers[currentQuestion])
+        {
+            Debug.Log("Boo!");
+            storyText.text = "Uh oh";
+        }
+    }
 }
--- Assets/Scripts/Cookies/CookieScript.cs
+++ Assets/Scripts/Cookies/CookieScript.cs
@@ -9,70 +9,33 @@ public class CookieScript : MonoBehaviour
     [SerializeField] private CookieGameManager gameManager;
     [SerializeField] private GameObject quizCard;
     [SerializeField] private TMP_Text quizText;
-    [SerializeField] private TMP_Text storyText;
-    
+
 
     public void CheckCookie()
     {
         if (gameManager.currentQuestion == cookieOrderNumber)
         {
-            Debug.Log("Question Number: " + cookieOrderNumber);
-            gameManager.currentQuestion++;
-            if (cookieOrderNumber == 1)
+            if (cookieOrderNumber == 0)
             {
                 quizCard.SetActive(true);
-                quizText.text = "Question Number: " + cookieOrderNumber;
+            }
+            else if (cookieOrderNumber == 1)
+            {
+                quizCard.SetActive(true);
             }
             else if (cookieOrderNumber == 2)
             {
                 quizCard.SetActive(true);
-                quizText.text = "Question Number: " + cookieOrderNumber;
             }
             else if (cookieOrderNumber == 3)
             {
                 quizCard.SetActive(true);
-                quizText.text = "Question Number: " + cookieOrderNumber;
             }
             else if (cookieOrderNumber == 4)
             {
                 quizCard.SetActive(true);
-                quizText.text = "Question Number: " + cookieOrderNumber;
-            }
-            else if (cookieOrderNumber == 5)
-            {
-                quizCard.SetActive(true);
-                quizText.text = "Question Number: " + cookieOrderNumber;
             }
             gameObject.SetActive(false);
-        }
-    }
-
-    public void CheckQuizAnswer()
-    {
-        if (gameManager.currentQuestion == 1)
-        {
-            quizCard.SetActive(true);
-            quizText.text = "Question Number: " + cookieOrderNumber;
-        }
-        else if (gameManager.currentQuestion == 2)
-        {
-            quizCard.SetActive(true);
-            quizText.text = "Question Number: " + cookieOrderNumber;
-        }
-        else if (gameManager.currentQuestion == 3)
-        {
-            quizCard.SetActive(true);
-            quizText.text = "Question Number: " + cookieOrderNumber;
-        }
-        else if (gameManager.currentQuestion == 4)
-        {
-            quizCard.SetActive(true);
-            quizText.text = "Question Number: " + cookieOrderNumber;
-        }
-        else if (gameManager.currentQuestion == 5)
-        {
-            quizCard.SetActive(true);
-            quizText.text = "Question Number: " + cookieOrderNumber;
         }
     }
 }
