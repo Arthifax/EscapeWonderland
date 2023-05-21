4ed869c2aa215a42abd14c682edd763d7f4af1e3
Arthifax <pepijnlinden@gmail.com>
2023-05-22 01:53:13 +0200

    Koningin game toegevoegd

--- Assets/Scripts/AnimateSprite.cs
+++ Assets/Scripts/AnimateSprite.cs
@@ -1,13 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class AnimateSprite : MonoBehaviour
 {
+    [SerializeField] private float current = -0.1f;
+    [SerializeField] private float target = 0f;
+    [SerializeField] private float speed = 0.5f;
+    [SerializeField] Material mat;
+    [SerializeField] GameObject background;
+
+    private void Start()
+    {
+        mat = GetComponent<Image>().material;
+    }
+
+    private void Update()
+    {
+        current = Mathf.MoveTowards(current, target, speed * Time.deltaTime);
+        mat.SetFloat("_FadeAmount", current);
+    }
 
     public void DissolveSprite()
     {
-        Material mat = GetComponent<Renderer>().material;
-        mat.SetFloat("_FadeAmount", 1.0f);
+        target = 1f;
     }
 }
